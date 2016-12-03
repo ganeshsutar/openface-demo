f4e9e6da544c910c8bb8a57ff4d94dcc8b7d1408
ganeshsutar <sutarganesh.mail@gmail.com>
2016-12-03 23:19:08 +0530

    Completed working

--- listdir.py
+++ listdir.py
@@ -1,7 +1,9 @@
 from os import walk
+import os
 
 def get_files(path):
     files = []
     for (dirpath, dirnames, filenames) in walk(path):
-        files.extend(filenames)
+        for file in filenames:
+            files.append(os.path.join(dirpath, file))
     return files
